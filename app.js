ccce3f4e151d89525cacd1643ce59f5b7c31dbcf
jaskaran5109 <67426155+jaskaran5109@users.noreply.github.com>
2022-09-13 16:46:05 +0530

    course edit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import payment from './routes/paymentRoutes.js'
 import other from './routes/otherRoutes.js'
 import ErrorMiddleware from './middlewares/Error.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 
 config({
   path: "./config/config.env",
@@ -15,6 +16,11 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true,
+  methods:['GET', 'POST',"PUT","DELETE"],
+}))
 
 app.use("/api/v1",course)
 app.use("/api/v1",user)
@@ -23,4 +29,5 @@ app.use("/api/v1",other)
 
 export default app;
 
+app.get("/",(req,res)=>res.send(`<h1>Site is working. Click <a href=${process.env.FRONTEND_URL}>here<a/>to visit frontend</h1>`))
 app.use(ErrorMiddleware)
