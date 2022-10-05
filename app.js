401dfa0ae6412173f0075aad0a2aed58685aba7c
jaskaran5109 <67426155+jaskaran5109@users.noreply.github.com>
2022-10-05 23:00:26 +0530

    notes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import course from './routes/courseRoutes.js'
 import user from './routes/userRoutes.js'
+import note from './routes/noteRoutes.js'
 import payment from './routes/paymentRoutes.js'
 import other from './routes/otherRoutes.js'
 import ErrorMiddleware from './middlewares/Error.js'
@@ -26,6 +27,7 @@ app.use("/api/v1",course)
 app.use("/api/v1",user)
 app.use("/api/v1",payment)
 app.use("/api/v1",other)
+app.use("/api/v1",note)
 
 export default app;
 
